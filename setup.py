2f793af8badcf9df9203881f521706a5bd163956
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-05-03 10:41:30 -0500

    defining source code structure for the project

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='mamluk-knowledgespace',
+      version='0.1',
+      description='A tool to parse the PDFs from the Mamluk Studies Journal for metadata and generate SAF imports for knowledgespace',
+      author='Tyler Danstrom',
+      author_email='tdanstrom@uchicago.edu',
+      url='https://github.com/uchicago-library/mamluk-knowledgespace-import/',
+      packages=['knowledgespace-import'],
+     )
